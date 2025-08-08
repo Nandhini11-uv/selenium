e1204ac0a4cecb28a18fd4b6a52ea15a132f0798
USER <USER@LAPTOP-6AITHIOJ>
2025-08-08 11:34:33 +0530

    created one more class

---
+++ Selenium_2.30/src/Purnita/changesbyAutomationLead.java
@@ -0,0 +1,17 @@
+package Purnita;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+@Test
+public class changesbyAutomationLead {
+	
+	WebDriver driver=new ChromeDriver();
+	
+   
+	
+	
+
+}
